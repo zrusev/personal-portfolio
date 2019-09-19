a42f7f97c0c162ac8cc68c5ad4b23dd27c0e9431
Zlatko Rusev <zrusev@r3aig.com>
2019-09-19 12:54:55 +0300

    ctx-service images path

--- src/services/ctx-service.js
+++ src/services/ctx-service.js
@@ -4,8 +4,9 @@ const obj = {};
 context.keys().forEach((key) => {
     const name = key.split('./').pop().substring(0, key.length - 6);
 
+    const imgPath = context(key).replace('../../', 'images/');
     obj[name] = process.env.NODE_ENV === 'production' 
-                    ? `/dist${context(key)}` 
+                    ? `${imgPath}` 
                     : context(key);
 });
 
