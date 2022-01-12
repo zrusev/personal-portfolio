9975b3b192115cc2a0992e04a9a3df18521210c5
Zlatko Rusev <zlatko.rusev@gmail.com>
2022-01-12 12:35:41 +0200

    Skills update

--- src/services/skills-service.js
+++ src/services/skills-service.js
@@ -2,33 +2,37 @@ export default () => Promise.resolve({
     data: [{
             id: 0,
             name: 'C#',
-            description: '.Net Core, Docker, Microservices, DDD, Jenkins, AWS'
+            description: '.Net Core, Docker, Microservices, DDD'
         }, {
             id: 1,
             name: 'JavaScript',
             description: 'ReactJS, TypeScript, Redux, Webpack'
         }, {
             id: 2,
+            name: 'CSS',
+            description: 'SCSS, SASS',
+        }, {
+            id: 3,
             name: 'SQL Server',
             description: 'EF Core, Dapper, T-SQL',
         }, {
-            id: 3,
+            id: 4,
             name: 'CI/CD',
             description: 'Jenkins, GIT'
-        } , {
-            id: 4,
-            name: 'CSS',
-            description: 'SCSS, SASS',
-        },{
+        }, {
             id: 5,
+            name: 'Cloud',
+            description: 'AWS, Azure'
+        }, {
+            id: 6,
             name: 'VBA',
             description: 'VBA, ADODB',
         }, {
-            id: 6,
+            id: 7,
             name: 'PowerBI',
             description: 'Power Automate',
         }, {
-            id: 7,
+            id: 8,
             name: 'Python',
             description: 'Pandas, NumPy',
         }]
